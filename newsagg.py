204c3f3517a5e8d839e2942ea930845053abc470
SlightlyUnorthodox <gerts.dax1@gmail.com>
2016-02-15 16:00:55 -0500

    minor fix, still testing multi-threaded downloading

--- newsagg.py
+++ newsagg.py
@@ -9,24 +9,27 @@ def main():
 
 	# Active list of news/media sources
 	
-	sources = ['http://cnn.com','http://foxnews.com',
-	'http://npr.org','http://msnbc.com','http://cbs.com','www.ap.org',
-	'http://economist.com','http://time.com','http://nytimes.com',
-	'http://slate.com','http://espn.com','http://reuters.com','http://usatoday.com'
-	'http://bbc.com','http://fivethirtyeight.com']
+	sources = ['http://fivethirtyeight.com','http://bbc.com']
+
+	#sources = ['http://cnn.com','http://foxnews.com',
+	#'http://npr.org','http://msnbc.com','http://cbs.com','www.ap.org',
+	#'http://economist.com','http://time.com','http://nytimes.com',
+	#'http://espn.com','http://reuters.com','http://usatoday.com'
+	#'http://bbc.com','http://fivethirtyeight.com']
 
 	papers = {} # Empty dictionary
 
 	# Build diction, using url name for keys ex/ 'http://cnn.com' key will be 'cnn'
 	for i in range(len(sources)):
 		papers[re.sub(r'(^https?:\/\/|\.com$|\.org$)','',sources[i])] = newspaper.build(sources[i],memoize_articles=False)
-	
-	# Print number of articles added from "recent" list for logging purposes
-	for i in papers:
-		print(i,papers[i].size())
+		# Print number of articles added from "recent" list for logging purposes
+		print(papers.items()[0][0],papers.items()[0][1].size())
+		print(i)
+
+	print("check")
 
 	# Download all articles via multi-threading
-	news_pool.set(papers, threads_per_source=1) # Test various thread counts
+	news_pool.set([x[1] for x in papers.items()], threads_per_source=1) # Test various thread counts
 	news_pool.join()
 
 	# Parse all articles
@@ -36,9 +39,9 @@ def main():
 			#papers[i].articles[j].download()
 			papers[i].articles[j].parse()
 
-	# Export articles to csv
+	# Append articles to csv
 	# Prototype format: col(1) = source, col(2) = title, col(3) = authors, col(4) = text
-	writer = csv.writer(open('papers.csv','wb'))
+	writer = csv.writer(open('papers.csv','a'))
 	for i in papers:
 		source = i
 		for j in range(papers[i].size()):
@@ -46,9 +49,10 @@ def main():
 			title = paper[i].articles[j].title
 			authors = paper[i].articles[j].authors
 			text = papers[i].articles[j].text
+			date = papers[i].articles[j].publish_date
 			# Identify keywords, while we're at it
 			keywords = papers[i].articles[j].keywords
-			writer.writerow([source,title,authors,text,keywords])
+			writer.writerow([source,date,title,authors,text,keywords])
 
 if __name__ == "__main__":
 	# Suppress "no handlers could be found" message for tldextract
