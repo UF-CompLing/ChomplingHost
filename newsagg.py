6aff658361ca4ff0e6ef72b493f286d8667fb775
SlightlyUnorthodox <gerts.dax1@gmail.com>
2016-02-20 19:38:03 -0500

    downloading and csv conversion debugged, downloading is still slow, unavoidable

--- newsagg.py
+++ newsagg.py
@@ -6,10 +6,12 @@ def main():
 	import newspaper
 	from newspaper import news_pool
 	import re
+	import csv
+	import unicodedata
 
 	# Active list of news/media sources
 	
-	sources = ['http://fivethirtyeight.com','http://bbc.com']
+	sources = ['http://fivethirtyeight.com']
 
 	#sources = ['http://cnn.com','http://foxnews.com',
 	#'http://npr.org','http://msnbc.com','http://cbs.com','www.ap.org',
@@ -19,6 +21,8 @@ def main():
 
 	papers = {} # Empty dictionary
 
+	print("Building papers\n....\n...\n...")
+
 	# Build diction, using url name for keys ex/ 'http://cnn.com' key will be 'cnn'
 	for i in range(len(sources)):
 		papers[re.sub(r'(^https?:\/\/|\.com$|\.org$)','',sources[i])] = newspaper.build(sources[i],memoize_articles=False)
@@ -26,11 +30,13 @@ def main():
 		print(papers.items()[0][0],papers.items()[0][1].size())
 		print(i)
 
-	print("check")
+	print("Downloading articles (this may take a while)\n...\n...\n...")
 
 	# Download all articles via multi-threading
 	news_pool.set([x[1] for x in papers.items()], threads_per_source=1) # Test various thread counts
 	news_pool.join()
+
+	print("Extracting text from articles \n...\n...\n...")
 
 	# Parse all articles
 	for i in papers:
@@ -39,20 +45,25 @@ def main():
 			#papers[i].articles[j].download()
 			papers[i].articles[j].parse()
 
+	print("Writing new articles to dump file \n...\n...\n...")
+
 	# Append articles to csv
 	# Prototype format: col(1) = source, col(2) = title, col(3) = authors, col(4) = text
-	writer = csv.writer(open('papers.csv','a'))
-	for i in papers:
-		source = i
-		for j in range(papers[i].size()):
-			# Grab key features
-			title = paper[i].articles[j].title
-			authors = paper[i].articles[j].authors
-			text = papers[i].articles[j].text
-			date = papers[i].articles[j].publish_date
-			# Identify keywords, while we're at it
-			keywords = papers[i].articles[j].keywords
-			writer.writerow([source,date,title,authors,text,keywords])
+	with open('papers.csv','wb') as outcsv:
+		writer = csv.writer(outcsv)
+		writer.writerow(["Source","Date","Title","Authors","Text","Keywords"])
+		for i in papers:
+			source = i
+			for j in range(papers[i].size()):
+				# Grab key features
+				title = unicodedata.normalize('NFKD',papers[i].articles[j].title).encode('ascii','ignore')
+				authors = [x.encode('UTF-8') for x in papers[i].articles[j].authors]
+				text = unicodedata.normalize('NFKD',papers[i].articles[j].text).encode('ascii','ignore')
+				date = papers[i].articles[j].publish_date
+				#date = unicodedata.normalize('NFKD',papers[i].articles[j].publish_date).encode('ascii','ignore')
+				# Identify keywords, while we're at it
+				keywords = [x.encode('UTF-8') for x in papers[i].articles[j].keywords]
+				writer.writerow([source,date,title,authors,text,keywords])
 
 if __name__ == "__main__":
 	# Suppress "no handlers could be found" message for tldextract
