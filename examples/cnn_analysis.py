5224dbf306b65be563a14d0b2a86e9329a76b5bf
Dax <gerts.dax1@gmail.com>
2016-03-10 15:51:59 -0500

    basic cnn analysis complete

--- examples/cnn_analysis.py
+++ examples/cnn_analysis.py
@@ -1,71 +1,82 @@
 # Title: CNN analysis
 # Author: Dax Gerts
 # Date: 24 February 2016
-# Descriptio
+# Description
 
 ## Modules
 
 import nltk, re
 from nltk.classify import NaiveBayesClassifier
-from nltk.corpus import subjectivity
+from nltk.corpus import subjectivity as sub
 from nltk.sentiment import SentimentAnalyzer
 from nltk.sentiment.util import *
 from nltk import word_tokenize
 
-## Build training set from nltk subjectivity/objectivity corpora
-training_subjective = [(sentences,'subj') for sentences in subjectivity.sents(categories='subj')[:5000]]
-training_objective = [(sentences,'obj') for sentences in subjectivity.sents(categories='obj')[:5000]]
-training = training_objective + training_subjective
+def utf_8_encoder(unicode_csv_data):
+	for line in unicode_csv_data:
+		yield line.encode('utf-8')
 
-# Build test set from CNN csv dump file
-test = [] #source = [],date = [],title =[],authors = [],keywords = []
+def training_setup():
+	## Build training set from nltk subjectivity/objectivity corpora
+	training_subjective = [(sentences,'subj') for sentences in sub.sents(categories='subj')[:500]]
+	training_objective = [(sentences,'obj') for sentences in sub.sents(categories='obj')[:500]]
+	training = training_objective + training_subjective
+	return training
 
-with open("cnn.csv") as csvfile:
-	reader = csv.DictReader(csvfile)
-	for row in reader:	
-		# Clean and separate text into sentence segments
-		
-		# Remove \n and \ characters
-		clean_text = re.sub(r'(\\)','',re.sub('(\n)','',row['Text']))
-		# Remove quotes
-		clean_text = re.sub('"(.*?)"','',clean_text)
-		#Tokenize strings
-		tokenized_text = word_tokenize(clean_text)
+def testing_setup():
+	# Build test set from CNN csv dump file
+	testing = [] #source = [],date = [],title =[],authors = [],keywords = []
 
-		# Segment phrases and convert to unicode
-		segments = []
-		for i in range(len(tokenized_text)):
-			segments.append(tokenized_text[i].encode("ascii"))		
+	with open("cnn.csv") as csvfile:
+		reader = csv.DictReader(utf_8_encoder(csvfile))
+		for row in reader:	
+			# Clean and separate text into sentence segments
+			
+			# Remove \n and \ characters
+			clean_text = re.sub(r'(\\)','',re.sub('(\n)','',unicode(row['Text'],'utf-8')))
+			# Remove quotes
+			clean_text = re.sub('"(.*?)"','',clean_text)
+			#Tokenize strings
+			tokenized_text = word_tokenize(clean_text)
 
-		test.append((segments,'na'))
+			# Segment phrases and convert to unicode
+			segments = []
+			for i in range(len(tokenized_text)):
+				segments.append(tokenized_text[i])		
 
-		# Other columns not used here
-		#keywords.append(row['Keywords'])
-		#source.append(row['Source'])
-		#date.append(row['Date'])
-		#title.append(row['Authors'])
+			testing.append((segments,"subj"))
 
-## Apply sentiment analysis to data to extract new "features"
+			#keywords.append(row['Keywords'])
+			#source.append(row['Source'])
+			#date.append(row['Date'])
+			#title.append(row['Authors'])
+	return testing
 
-# Initialize sentiment analyzer object
-sentiment_analyzer = SentimentAnalyzer()
+def build_and_test_model(training,testing):
+	## Apply sentiment analysis to data to extract new "features"
 
-# Mark all negative words in training data, using existing list of negative words
-all_negative_words = sentiment_analyzer.all_words([mark_negation(data) for data in training])
+	# Initialize sentiment analyzer object
+	sentiment_analyzer = SentimentAnalyzer()
 
-unigram_features = sentiment_analyzer.unigram_word_feats(all_negative_words, min_freq=4)
-len(unigram_features)
-sentiment_analyzer.add_feat_extractor(extract_unigram_feats,unigrams=unigram_features)
+	# Mark all negative words in training data, using existing list of negative words
+	all_negative_words = sentiment_analyzer.all_words([mark_negation(data) for data in training])
 
-training_final = sentiment_analyzer.apply_features(training)
-test_final = sentiment_analyzer.apply_features(test)
+	unigram_features = sentiment_analyzer.unigram_word_feats(all_negative_words, min_freq=4)
+	len(unigram_features)
+	sentiment_analyzer.add_feat_extractor(extract_unigram_feats,unigrams=unigram_features)
 
-## Traing model and test
+	training_final = sentiment_analyzer.apply_features(training)
+	testing_final = sentiment_analyzer.apply_features(testing)
+	
+	## Traing model and test
 
-model = NaiveBayesClassifier.train
-classifer = sentiment_analyzer.train(model, training_final)
+	model = NaiveBayesClassifier.train
+	classifer = sentiment_analyzer.train(model, training_final)
 
-for key, value in sorted(sentiment_analyzer.evaluate(test_final).items()):
-    print('{0}: {1}'.format(key,value))
+	for key, value in sorted(sentiment_analyzer.evaluate(testing_final).items()):
+	    print('{0}: {1}'.format(key,value))
 
-
+if __name__ == "__main__":
+	training = training_setup()
+	testing = testing_setup()
+	build_and_test_model(training=training,testing=testing)
