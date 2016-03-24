df2d2b48d55cfac97c282b9afd6f47caa2bf8d7b
SlightlyUnorthodox <gerts.dax1@gmail.com>
2016-03-24 17:14:19 -0400

    semi-demo ready

--- news/views.py
+++ news/views.py
@@ -15,6 +15,12 @@ from .models import Article
 import datetime
 import decimal
 import operator
+import nltk, re
+from nltk.classify import NaiveBayesClassifier
+from nltk.corpus import subjectivity
+from nltk.sentiment import SentimentAnalyzer
+from nltk.sentiment.util import *
+from nltk import word_tokenize
 
 def index(request):
 	article_list = Article.objects.order_by('article_id')
@@ -54,6 +60,53 @@ def search(request):
 	context = RequestContext(request)
 	return render_to_response('news.html', {"results" : results}, context_instance=context)
 
+def classifier(article_text):
+
+	# Size of dataset(s)
+
+	n = 500
+	print(article_text)
+	# Get "n" subjective and objective phrases from subjectivity corpus
+
+	subjective = [(sentences,'subj') for sentences in subjectivity.sents(categories='subj')[:n]]
+	objective = [(sentences,'obj') for sentences in subjectivity.sents(categories='obj')[:n]]
+
+	training_subjective = subjective[:int(.8*n)]
+	test_subjective = subjective[int(.8*n):n]
+	training_objective = objective[:int(.8*n)]
+	test_objective = objective[int(.8*n):n]
+
+	training = training_subjective + training_objective
+	test = test_subjective + test_objective
+
+	## Apply sentiment analysis to data to extract new "features"
+
+	# Initialize sentiment analyzer object
+
+	sentiment_analyzer = SentimentAnalyzer()
+
+	# Mark all negative words in training data, using existing list of negative words
+
+	all_negative_words = sentiment_analyzer.all_words([mark_negation(data) for data in training])
+
+	unigram_features = sentiment_analyzer.unigram_word_feats(all_negative_words, min_freq=4)
+	len(unigram_features)
+	sentiment_analyzer.add_feat_extractor(extract_unigram_feats,unigrams=unigram_features)
+
+	training_final = sentiment_analyzer.apply_features(training)
+	test_final = sentiment_analyzer.apply_features(test)
+
+	## Traing model and test
+
+	model = NaiveBayesClassifier.train
+	classifer = sentiment_analyzer.train(model, training_final)
+
+	output = ""
+	for key, value in sorted(sentiment_analyzer.evaluate(test_final).items()):
+	    output = output + str('{0}: {1}'.format(key,value)) + "<br>"
+
+	return output
+
 def analyze(request):
 	query = request.GET.get('q')
 	sort = request.GET.get('sort')
@@ -67,7 +120,7 @@ def analyze(request):
 	trash = request.GET.get('trash')
 	length = request.GET.get('length')
 	# Source Options
-	sources = ['cnn','foxnews','npr','msnbc','ap','economist','time','fivethirtyeight','nytimes','espn','onion']
+	sources = ['cnn','fox','npr','msnbc','ap','economist','time','fivethirtyeight','nytimes','espn','onion']
 	# Procedures Options
 	classify = request.GET.get('classify')
 	polarity = request.GET.get('polarity')
@@ -79,23 +132,29 @@ def analyze(request):
 		if request.GET.get(sources[i]) != None:
 			sources2.append(sources[i])
 			print("check one")
-	query_set = reduce(operator.or_, (Q(article_source__icontains=item) for item in sources2))
-	results = Article.objects.filter(query_set)
-	#results = results.filter({'article_source__icontains': sources2}) 
-
+	if len(sources2) != 0:
+		query_set = reduce(operator.or_, (Q(article_source__icontains=item) for item in sources2))
+		results = Article.objects.filter(query_set)
+		if query != None:
+			results = results.filter(**{'article_title__icontains': str(query)}) 
+	else:
+		results = None
 	# Clean data
 	if trash != None:
 		print("trash removed")
 	if length != None:
 		print("short articles removed")
 
+	output = "No dice, you gotta pick some data"
+
 	# Run analysis
 	if classify != None:
 		# run classifier
+		output = classifier(article_text = results.values_list('article_text',flat=True))
 		print("classification analyzed")
 	if polarity != None:
 		# run polarity solver
 		print("polarity analyzed")
 
 	context = RequestContext(request)
-	return render_to_response('news.html', {"results" : results}, context_instance=context)
+	return render_to_response('news.html', {"results" : results,"output" : output}, context_instance=context)
